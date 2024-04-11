c16c6b8b952dcc42179f4b6de150a393b05ebfa5
Darek Mazur <dev@darekmazur.it>
2024-04-11 02:30:14 +0200

    Add font awesome

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,4 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fab, fas, far);
 
 const GlobalStyle = createGlobalStyle`
 
