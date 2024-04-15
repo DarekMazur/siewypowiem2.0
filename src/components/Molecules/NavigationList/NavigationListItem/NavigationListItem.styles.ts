90ef97ad9283f9b7c2fdaf056bf4c8d9f27d4eef
Darek Mazur <dev@darekmazur.it>
2024-04-15 05:00:26 +0200

    Refactor current route link style

--- src/components/Molecules/NavigationList/NavigationListItem/NavigationListItem.styles.ts
+++ src/components/Molecules/NavigationList/NavigationListItem/NavigationListItem.styles.ts
@@ -7,8 +7,22 @@ interface IStyledNavigationListItem {
 export const StyledNavigationListItem = styled.li<IStyledNavigationListItem>`
   margin: 0 4rem 0 0;
   padding-bottom: 0.5rem;
-  border-bottom: ${({ $isCurrent, theme }) =>
-    $isCurrent ? `0.2rem solid ${theme.colors.blue}` : 'none'};
+
+  &::after {
+    content: '';
+    display: block;
+    height: 0.2rem;
+    width: 110%;
+    transform: ${({ $isCurrent }) =>
+      $isCurrent ? `scaleX(1) translateX(-5%)` : `scaleX(0) translateX(-5%)`};
+    background-color: ${({ theme }) => theme.colors.blue};
+    transform-origin: 0 0;
+    transition: transform 200ms ease-in-out;
+  }
+
+  &:hover::after {
+    transform: scaleX(1) translateX(-5%);
+  }
 
   &:last-of-type {
     margin: 0;
