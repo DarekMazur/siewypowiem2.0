d2c0bdeac361520f7c5399f1f8fd115b11b4e4a6
Darek Mazur <dev@darekmazur.it>
2024-04-27 03:48:14 +0200

    Add dynamic data to hero

--- src/hooks/useSlogan.ts
+++ src/hooks/useSlogan.ts
@@ -1,13 +1,31 @@
 import { slogan } from '@/utils/data';
+import { ICategoryTypes, IUserTypes } from '@/utils/types';
 import { usePathname } from 'next/navigation';
 
 // eslint-disable-next-line consistent-return
-export const useSlogan = () => {
+export const useSlogan = (data?: ICategoryTypes | IUserTypes) => {
   const pathname = usePathname();
 
-  switch (pathname) {
-    case '/blog':
-      return slogan.blog;
+  if (pathname === '/blog') {
+    return slogan.blog;
+  }
+
+  const dynamic = pathname.split('/').slice(1);
+  const dynamicSlogan = {
+    title: '',
+    slogan:
+      data && data.attributes && dynamic[0] === 'categories'
+        ? data.attributes.description
+        : '',
+  };
+
+  switch (dynamic[0]) {
+    case 'categories':
+      dynamicSlogan.title = `Category ${(data && data.attributes && data.attributes.title) || ''}`;
+      return dynamicSlogan;
+    case 'authors':
+      dynamicSlogan.title = `About ${(data && data.username) || ''}`;
+      return dynamicSlogan;
     default:
       break;
   }
