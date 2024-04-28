84a327f3f3abc28348a9708959e58a2e265b0531
Darek Mazur <dev@darekmazur.it>
2024-04-28 02:44:17 +0200

    Create universal PageView component

---
+++ src/components/Views/PageView/PageView.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const MainWrapper = styled.main`
+  padding-bottom: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem 2rem;
+`;
