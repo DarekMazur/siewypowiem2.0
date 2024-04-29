a06e7976432e7734419bcb15d28fd472210dcacb
Darek Mazur <dev@darekmazur.it>
2024-04-29 20:40:25 +0200

    Add article navigation

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -5,7 +5,7 @@ import slugify from 'slugify';
 const Article = async ({ params }: { params: { slug: string } }) => {
   async function getData() {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/articles?populate=*pagination[pageSize]=9999`,
+      `${process.env.NEXT_PUBLIC_API_URL}/api/articles?populate=*&sort=publishedAt:desc&pagination[pageSize]=9999`,
       {
         cache: 'no-store',
       },
@@ -18,13 +18,35 @@ const Article = async ({ params }: { params: { slug: string } }) => {
     return res.json();
   }
 
+  let currentIndex: number = -1;
+
   const allArticles: { data: Array<IArticleTypes> } = await getData();
   const currentArticle = allArticles.data.filter(
     (article) =>
       slugify(article.attributes.title, { lower: true }) === params.slug,
   );
 
-  return <ArticleView article={currentArticle[0]} />;
+  allArticles.data.forEach((article, index) => {
+    if (slugify(article.attributes.title, { lower: true }) === params.slug) {
+      currentIndex = index;
+    }
+  });
+
+  const nextArticle =
+    currentIndex !== 0 ? allArticles.data[currentIndex - 1] : null;
+
+  const previousArticle =
+    currentIndex !== allArticles.data.length - 1
+      ? allArticles.data[currentIndex + 1]
+      : null;
+
+  return (
+    <ArticleView
+      article={currentArticle[0]}
+      next={nextArticle?.attributes.title}
+      previous={previousArticle?.attributes.title}
+    />
+  );
 };
 
 export default Article;
--- src/components/Views/ArticleView/ArticleView.tsx
+++ src/components/Views/ArticleView/ArticleView.tsx
@@ -1,13 +1,36 @@
 'use client';
 
+/* eslint-disable react/no-danger */
+import { FC } from 'react';
+import Link from 'next/link';
+import slugify from 'slugify';
 import { IArticleTypes } from '@/utils/types';
+import { MainWrapper } from '../PageView/PageView.styles';
 
-const ArticleView = ({ article }: { article: IArticleTypes }) => {
+interface IArticleViewProps {
+  article: IArticleTypes;
+  next?: string;
+  previous?: string;
+}
+
+const ArticleView: FC<IArticleViewProps> = ({ article, next, previous }) => {
   return (
-    <main>
-      <h2>{article.attributes.title}</h2>
-      <p>{article.attributes.body}</p>
-    </main>
+    <MainWrapper>
+      <section dangerouslySetInnerHTML={{ __html: article.attributes.body }} />
+      <section>
+        {previous ? (
+          <Link href={`/article/${slugify(previous, { lower: true })}`}>
+            <button type='button'>Previous: {previous}</button>
+          </Link>
+        ) : null}
+
+        {next ? (
+          <Link href={`/article/${slugify(next, { lower: true })}`}>
+            <button type='button'>Next: {next}</button>
+          </Link>
+        ) : null}
+      </section>
+    </MainWrapper>
   );
 };
 
