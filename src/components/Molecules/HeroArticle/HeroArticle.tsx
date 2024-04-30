9c93f2f173bfb05c96385c37ca53935eb8fa96f0
Darek Mazur <dev@darekmazur.it>
2024-04-30 04:58:46 +0200

    Add workink links to hero article

--- src/components/Molecules/HeroArticle/HeroArticle.tsx
+++ src/components/Molecules/HeroArticle/HeroArticle.tsx
@@ -1,59 +1,88 @@
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import slugify from 'slugify';
 import ArticleDate from '@/components/Atoms/ArticleDate/ArticleDate';
 import { Handwritting } from '@/components/Atoms/Handwritting/Handwritting.styles';
 import HeroTitle from '@/components/Atoms/HeroTitle/HeroTitle';
 import HeroCategories from '@/components/Atoms/HeroCategories/HeroCategories';
 import { useGetArticlesQuery, useGetLastArticleQuery } from '@/store';
 import { dateFormat } from '@/utils/dateFormat';
-import Link from 'next/link';
+import { StyledLink } from '@/components/Atoms/Link/Link.styles';
 import { StyledHeroArticle } from './HeroArticle.styles';
 
-const HeroArticle = ({ uuid }: { uuid?: string }) => {
+const HeroArticleContent = ({ uuid }: { uuid?: string }) => {
   const { data: article } = useGetLastArticleQuery();
   const { data: singleArticle } = useGetArticlesQuery({ uuid });
+  return (
+    <>
+      {article ? (
+        <>
+          <HeroTitle
+            title={
+              singleArticle?.data.length === 1
+                ? singleArticle?.data[0].attributes.title
+                : article.data[0].attributes.title
+            }
+          />
+          <Handwritting>
+            by{' '}
+            <StyledLink
+              href={`/authors/${singleArticle?.data.length === 1 ? slugify(singleArticle?.data[0].attributes.author.data.attributes.username, { lower: true }) : slugify(article.data[0].attributes.author.data.attributes.username, { lower: true })}`}
+              target='_blanc'
+              style={{ padding: 0 }}
+            >
+              {singleArticle?.data.length === 1
+                ? singleArticle?.data[0].attributes.author.data.attributes
+                    .username
+                : article.data[0].attributes.author.data.attributes.username}
+            </StyledLink>
+          </Handwritting>
+          {article.data[0].attributes.categories.data.length > 0 ||
+          (singleArticle &&
+            singleArticle.data[0].attributes.categories.data.length > 0) ? (
+            <HeroCategories
+              categories={
+                singleArticle?.data.length === 1 &&
+                singleArticle.data[0].attributes.categories.data.length > 0
+                  ? singleArticle.data[0].attributes.categories.data
+                  : article.data[0].attributes.categories.data
+              }
+            />
+          ) : null}
+          <ArticleDate
+            date={dateFormat(
+              singleArticle?.data.length === 1
+                ? singleArticle?.data[0].attributes.publishedAt
+                : article.data[0].attributes.publishedAt,
+            )}
+          />
+        </>
+      ) : (
+        <p>Nothing to see...</p>
+      )}
+    </>
+  );
+};
+
+const HeroArticle = ({ uuid }: { uuid?: string }) => {
+  const pathname = usePathname();
+  const { data: article } = useGetLastArticleQuery();
 
   return (
     <>
       {article ? (
         <StyledHeroArticle>
-          <Link href='/'>
-            <HeroTitle
-              title={
-                singleArticle?.data.length === 1
-                  ? singleArticle?.data[0].attributes.title
-                  : article.data[0].attributes.title
-              }
-            />
-            <Handwritting>
-              by{' '}
-              {singleArticle?.data.length === 1
-                ? singleArticle?.data[0].attributes.author.data.attributes
-                    .username
-                : article.data[0].attributes.author.data.attributes.username}
-            </Handwritting>
-            {article.data[0].attributes.categories.data.length > 0 ||
-            (singleArticle &&
-              singleArticle.data[0].attributes.categories.data.length > 0) ? (
-              <HeroCategories
-                categories={
-                  singleArticle?.data.length === 1 &&
-                  singleArticle.data[0].attributes.categories.data.length > 0
-                    ? singleArticle.data[0].attributes.categories.data
-                    : article.data[0].attributes.categories.data
-                }
-              />
-            ) : null}
-            <ArticleDate
-              date={dateFormat(
-                singleArticle?.data.length === 1
-                  ? singleArticle?.data[0].attributes.publishedAt
-                  : article.data[0].attributes.publishedAt,
-              )}
-            />
-          </Link>
+          {pathname === '/' ? (
+            <Link
+              href={`/article/${slugify(article.data[0].attributes.title, { lower: true })}`}
+            >
+              <HeroArticleContent />
+            </Link>
+          ) : (
+            <HeroArticleContent uuid={uuid} />
+          )}
         </StyledHeroArticle>
-      ) : (
-        <p>Nothing to see...</p>
-      )}
+      ) : null}
     </>
   );
 };
