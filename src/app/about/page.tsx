406a24a22cb0a11eccaa68960f1e5dbd5da42965
Darek Mazur <dev@darekmazur.it>
2024-05-01 07:07:07 +0200

    Style about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,83 +1,12 @@
 'use client';
 
-/* eslint-disable react/no-danger */
-import Image from 'next/image';
 import { MainWrapper } from '@/components/Views/PageView/PageView.styles';
-import { about, contact } from '@/utils/data';
-import { StyledLink } from '@/components/Atoms/Link/Link.styles';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import AboutView from '@/components/Views/AboutView/AboutView';
 
 const About = () => {
   return (
     <MainWrapper>
-      <section style={{ display: 'flex', gap: '2rem' }}>
-        <div
-          style={{ position: 'relative', minWidth: '20rem', height: '30rem' }}
-        >
-          <Image
-            src={about.image}
-            alt={about.name}
-            fill
-            style={{ objectFit: 'cover' }}
-          />
-        </div>
-        <div>
-          <h3>{about.greetings}</h3>
-          <div dangerouslySetInnerHTML={{ __html: about.body }} />
-          <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-            {about.inspirations.length > 0 ? (
-              <div>
-                <h4>My other projects:</h4>
-                <ul style={{ listStyle: 'none', padding: '0', margin: '0' }}>
-                  {about.inspirations.map((inspiring) => (
-                    <StyledLink key={inspiring.title} href={inspiring.url}>
-                      <li style={{ padding: '1rem 0' }}>{inspiring.title}</li>
-                    </StyledLink>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-            {about.links.length > 0 ? (
-              <div>
-                <h4>Recommendations:</h4>
-                <ul style={{ listStyle: 'none', padding: '0', margin: '0' }}>
-                  {about.links.map((link) => (
-                    <StyledLink key={link.title} href={link.url}>
-                      <li style={{ padding: '1rem 0' }}>{link.title}</li>
-                    </StyledLink>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-        <div style={{ minWidth: '30rem' }}>
-          <h3>Contact</h3>
-          <div
-            style={{
-              display: 'flex',
-              gap: '1rem',
-              justifyContent: 'space-evenly',
-              padding: '2rem 0 0',
-            }}
-          >
-            {contact.map((contactItem) => (
-              <StyledLink
-                key={contactItem.type}
-                $color='blue'
-                href={contactItem.link}
-                target='_blank'
-                rel='noreferer'
-              >
-                <FontAwesomeIcon
-                  icon={[contactItem.icon.prefix, contactItem.icon.name]}
-                  style={{ fontSize: '3rem' }}
-                />
-              </StyledLink>
-            ))}
-          </div>
-        </div>
-      </section>
+      <AboutView />
     </MainWrapper>
   );
 };
---
+++ src/components/Views/AboutView/AboutView.tsx
@@ -0,0 +1,74 @@
+/* eslint-disable react/no-danger */
+import Image from 'next/image';
+import { about, contact } from '@/utils/data';
+import { StyledLink } from '@/components/Atoms/Link/Link.styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  AboutContactSection,
+  AboutImage,
+  AboutLinkSection,
+  AboutViewWrapper,
+} from './AboutView.styles';
+
+const AboutView = () => {
+  return (
+    <>
+      <AboutViewWrapper>
+        <AboutImage>
+          <Image src={about.image} alt={about.name} fill />
+        </AboutImage>
+        <div>
+          <h3>{about.greetings}</h3>
+          <div dangerouslySetInnerHTML={{ __html: about.body }} />
+          <AboutLinkSection>
+            {about.inspirations.length > 0 ? (
+              <div>
+                <h4>My other projects:</h4>
+                <ul>
+                  {about.inspirations.map((inspiring) => (
+                    <StyledLink key={inspiring.title} href={inspiring.url}>
+                      <li>{inspiring.title}</li>
+                    </StyledLink>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+            {about.links.length > 0 ? (
+              <div>
+                <h4>Recommendations:</h4>
+                <ul>
+                  {about.links.map((link) => (
+                    <StyledLink key={link.title} href={link.url}>
+                      <li>{link.title}</li>
+                    </StyledLink>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </AboutLinkSection>
+        </div>
+        <AboutContactSection>
+          <h3>Contact</h3>
+          <div>
+            {contact.map((contactItem) => (
+              <StyledLink
+                key={contactItem.type}
+                $color='blue'
+                href={contactItem.link}
+                target='_blank'
+                rel='noreferer'
+              >
+                <FontAwesomeIcon
+                  icon={[contactItem.icon.prefix, contactItem.icon.name]}
+                  style={{ fontSize: '3rem' }}
+                />
+              </StyledLink>
+            ))}
+          </div>
+        </AboutContactSection>
+      </AboutViewWrapper>
+    </>
+  );
+};
+
+export default AboutView;
